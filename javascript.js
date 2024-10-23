bdb6d145bc611395bea17688555487d0cdaed884
Bharath-Kumar11 <pbhkr11@gmail.com>
2024-10-23 13:15:50 +0530

    added

---
+++ javascript.js
@@ -0,0 +1,11 @@
+function greetThePlanet(){
+    return "Hello World!" ;
+}
+<script>
+ function square(number){
+    let x = number * number
+ 
+let x = square(16);
+console.log(number);&rbrace;}
+ 
+</script>
